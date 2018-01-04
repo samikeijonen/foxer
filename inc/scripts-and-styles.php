f5e5dc2733d08d871ec607534732ad30353b0f57
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-01-04 14:26:12 +0200

    Add minified style.min.css using stylesheet_uri filter.

--- inc/scripts-and-styles.php
+++ inc/scripts-and-styles.php
@@ -23,6 +23,51 @@ function _s_scripts() {
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
 
 /**
+ * Filters the 'stylesheet_uri' to allow theme developers to offer a minimized version of their main
+ * 'style.css' file. It will detect if a 'style.min.css' file is available and use it if SCRIPT_DEBUG
+ * is disabled.
+ *
+ * @author    Justin Tadlock <justin@justintadlock.com>
+ * @copyright Copyright (c) 2008 - 2017, Justin Tadlock
+ * @link      http://themehybrid.com/hybrid-core
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @param     string  $stylesheet_uri      The URI of the active theme's stylesheet.
+ * @param     string  $stylesheet_dir_uri  The directory URI of the active theme's stylesheet.
+ * @return    string  $stylesheet_uri
+ */
+function _s_min_stylesheet_uri( $stylesheet_uri, $stylesheet_dir_uri ) {
+	// Get '.min' suffix.
+	$suffix = _s_get_min_suffix();
+
+	// Use the .min stylesheet if available.
+	if ( $suffix ) {
+		// Remove the stylesheet directory URI from the file name.
+		$stylesheet = str_replace( trailingslashit( $stylesheet_dir_uri ), '', $stylesheet_uri );
+
+		// Change the stylesheet name to 'style.min.css'.
+		$stylesheet = str_replace( '.css', "{$suffix}.css", $stylesheet );
+
+		// If the stylesheet exists in the stylesheet directory, set the stylesheet URI to the dev stylesheet.
+		if ( file_exists( trailingslashit( get_stylesheet_directory() ) . $stylesheet ) ) {
+			$stylesheet_uri = esc_url( trailingslashit( $stylesheet_dir_uri ) . $stylesheet );
+		}
+	}
+
+	// Return the theme stylesheet.
+	return $stylesheet_uri;
+}
+add_filter( 'stylesheet_uri', '_s_min_stylesheet_uri', 5, 2 );
+
+/**
+ * Helper function for getting the script/style `.min` suffix for minified files.
+ *
+ * @return string
+ */
+function _s_get_min_suffix() {
+	return defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ? '' : '.min';
+}
+
+/**
  * Handles JavaScript detection.
  *
  * Adds a `js` class to the root `<html>` element when JavaScript is detected.
@@ -31,13 +76,3 @@ function _s_javascript_detection() {
 	echo "<script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>\n";
 }
 add_action( 'wp_head', '_s_javascript_detection', 0 );
-
-/**
- * Helper function for getting the script/style `.min` suffix for minified files.
- *
- * @since  1.0.0
- * @return string
- */
-function _s_get_min_suffix() {
-	return defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ? '' : '.min';
-}
