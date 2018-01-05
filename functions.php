021f74ddcaf1a86aaf9357cde12f047b2576d2ba
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-01-05 12:06:20 +0200

    Remove support for Jetpack and Woocommerce.

--- functions.php
+++ functions.php
@@ -147,17 +147,3 @@ require get_template_directory() . '/inc/template-functions.php';
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
-/**
- * Load WooCommerce compatibility file.
- */
-if ( class_exists( 'WooCommerce' ) ) {
-	require get_template_directory() . '/inc/woocommerce.php';
-}
