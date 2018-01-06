8463ecbbf26341562ec30e7c39a9030fd37f1c81
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-01-06 13:29:33 +0200

    Remove pluggable function checks. These should always be possible via hooks, filters, and custom functions.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -28,45 +28,43 @@ function foxer_custom_header_setup() {
 }
 add_action( 'after_setup_theme', 'foxer_custom_header_setup' );
 
-if ( ! function_exists( 'foxer_header_style' ) ) :
-	/**
-	 * Styles the header image and text displayed on the blog.
-	 *
-	 * @see foxer_custom_header_setup().
-	 */
-	function foxer_header_style() {
-		$header_text_color = get_header_textcolor();
+/**
+ * Styles the header image and text displayed on the blog.
+ *
+ * @see foxer_custom_header_setup().
+ */
+function foxer_header_style() {
+	$header_text_color = get_header_textcolor();
 
-		/*
-		 * If no custom options for text are set, let's bail.
-		 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
-		 */
-		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
-			return;
-		}
-
-		// If we get this far, we have custom styles. Let's do this.
-		?>
-		<style type="text/css">
-		<?php
-		// Has the text been hidden?
-		if ( ! display_header_text() ) :
-		?>
-			.site-title,
-			.site-description {
-				position: absolute;
-				clip: rect(1px, 1px, 1px, 1px);
-			}
-		<?php
-			// If the user has set a custom color for the text use that.
-			else :
-		?>
-			.site-title a,
-			.site-description {
-				color: #<?php echo esc_attr( $header_text_color ); ?>;
-			}
-		<?php endif; ?>
-		</style>
-		<?php
+	/*
+		* If no custom options for text are set, let's bail.
+		* get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
+		*/
+	if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
+		return;
 	}
-endif;
+
+	// If we get this far, we have custom styles. Let's do this.
+	?>
+	<style type="text/css">
+	<?php
+	// Has the text been hidden?
+	if ( ! display_header_text() ) :
+	?>
+		.site-title,
+		.site-description {
+			position: absolute;
+			clip: rect(1px, 1px, 1px, 1px);
+		}
+	<?php
+		// If the user has set a custom color for the text use that.
+		else :
+	?>
+		.site-title a,
+		.site-description {
+			color: #<?php echo esc_attr( $header_text_color ); ?>;
+		}
+	<?php endif; ?>
+	</style>
+	<?php
+}
