faf994285e194fe55e422a4ef0713bb52cef2287
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-01-07 15:45:56 +0200

    Post Type, not Post Format.

--- archive.php
+++ archive.php
@@ -27,7 +27,7 @@ get_header(); ?>
 			/*
 				* Include the Post-Type-specific template for the content.
 				* If you want to override this in a child theme, then include a file
-				* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				* called content-___.php (where ___ is the Type Format name) and that will be used instead.
 				*/
 			get_template_part( 'template-parts/content', get_post_type() );
 
