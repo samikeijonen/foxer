53f51342ee3a94a0d7ba29d523c4fd20577751e3
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-01-07 16:02:09 +0200

    Remove aria-hidden from focusable element. Re-think this later, more info: https://make.wordpress.org/accessibility/handbook/best-practices/markup/post-excerpts-for-an-archive-template/

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -117,7 +117,7 @@ function foxer_post_thumbnail() {
 
 	<?php else : ?>
 
-	<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
+	<a class="post-thumbnail" href="<?php the_permalink(); ?>">
 		<?php
 			the_post_thumbnail( 'post-thumbnail', array(
 				'alt' => the_title_attribute( array(
