e25e450854ab3a11aba33f5ea2ce8bdc032f0708
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-03-08 18:34:36 +0200

    Remove emoji styles by Core and add own.

--- inc/scripts-and-styles.php
+++ inc/scripts-and-styles.php
@@ -31,6 +31,9 @@ function foxer_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+	// Remove the default emoji styles. We'll handle this in the stylesheet.
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
 }
 add_action( 'wp_enqueue_scripts', 'foxer_scripts' );
 
